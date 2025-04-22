33d89c03281912cab84be2ac191a1d84a5fed8d3
YeShenyu <769273229@qq.com>
2025-04-22 14:10:10 +0800

    添加Real类中的注释

--- 编译原理/Real.h
+++ 编译原理/Real.h
@@ -3,10 +3,11 @@
 #include "Tag.h"
 namespace Lexer {
 	// 类 Real 用于处理浮点数
+	//包括float类型和double类型
 	class Real : public Token
 	{
 	public:
-		const float value;
+		const double value;
 
 		Real(float v) : value(v), Token(Tag::REAL) {}
 
