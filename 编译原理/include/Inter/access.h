d5375e3f96b07cb6d5ea0d66ed6e29d6908f2b53
RH <2844417786@qq.com>
2025-05-02 17:44:28 +0800

    1

--- 编译原理/include/Inter/access.h
+++ 编译原理/include/Inter/access.h
@@ -18,7 +18,7 @@ namespace Inter
         Access(Id* a, Expr* i, Symbols::Type* p)
             : Op(Lexer::Word("[]", Lexer::Tag::INDEX), p), array(a), index(i) {}
 
-        Expr* gen(){ return new Access(array, index->reduce(), *type); }
+        Expr* gen(){ return new Access(array, &(index->reduce()), type); }
 
         void jumping(int t, int f){ emitjumps(reduce()->toString(), t, f); }
 
